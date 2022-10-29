fba4ec6d0386d8cd9c149bc034d9b0ca0470f9f3
dihnihassan <37321116+dihnihassan@users.noreply.github.com>
2022-10-28 20:57:24 -0700

    Update loopy-lighthouse.js

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,11 +1,14 @@
-for (let a = 100; a <= 200; a ++) {
-  if(a % 3 === 0) {
-    console.log("Loopy");
+for (let a = 100; a <= 200; a++) {
+  if (a % 3 === 0 && a % 4 === 0) {
+    console.log("LoopyLighthouse")
   }
   else if (a % 4 === 0){
-   console.log("Lighthouse");
+   console.log("Lighthouse")
   }
-  else if (a % 3 === 0 && a % 4 === 0);{
-    console.log("LoopyLighthouse");
+  else if (a % 3 === 0){
+    console.log("Loopy")
  }
+  else {
+    console.log(a);
+  }
 }
